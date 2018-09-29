013b19030023eaffe16099dc673395b1bacd70f8
Ahmed Jawad <ahmed.jawad.external@telefonica.com>
2018-09-29 14:10:22 +0200

    ivy counsel window number working correctly

--- init.el
+++ init.el
@@ -5,8 +5,9 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 
-(package-initialize)
-(setq package-list '(zenburn-theme auto-complete exec-path-from-shell  ess-view ess-R-data-view ensime sbt-mode scala-mode ))
+
+(setq package-list '(zenburn-theme auto-complete exec-path-from-shell  ess-view
+				   ess-R-data-view ensime sbt-mode scala-mode winum helm ivy counsel))
 
 (require 'package)
 (setq
@@ -19,11 +20,11 @@ package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 
                     ("melpa-stable" . "http://stable.melpa.org/packages/"))
 
-package-archive-priorities '(("melpa-stable" . 1)))
+package-archive-priorities '(("melpa" . 1)))
 
 ; activate all the packages (in particular autoloads)
 
-
+(package-initialize)
 ; fetch the list of packages available 
 (unless package-archive-contents
   (package-refresh-contents))
@@ -33,8 +34,48 @@ package-archive-priorities '(("melpa-stable" . 1)))
   (unless (package-installed-p package)
     (package-install package)))
 
+(counsel-mode 1)
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+
+
+
+(setq winum-keymap
+    (let ((map (make-sparse-keymap)))
+      (define-key map (kbd "C-`") 'winum-select-window-by-number)
+      (define-key map (kbd "C-²") 'winum-select-window-by-number)
+      (define-key map (kbd "M-0") 'winum-select-window-0-or-10)
+      (define-key map (kbd "M-1") 'winum-select-window-1)
+      (define-key map (kbd "M-2") 'winum-select-window-2)
+      (define-key map (kbd "M-3") 'winum-select-window-3)
+      (define-key map (kbd "M-4") 'winum-select-window-4)
+      (define-key map (kbd "M-5") 'winum-select-window-5)
+      (define-key map (kbd "M-6") 'winum-select-window-6)
+      (define-key map (kbd "M-7") 'winum-select-window-7)
+      (define-key map (kbd "M-8") 'winum-select-window-8)
+      map))
+(winum-mode)
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
 (load-theme 'zenburn t)
-(require 'exec-path-from-shell)
 (when (memq window-system '(mac ns))
       (exec-path-from-shell-initialize))
 
@@ -56,16 +97,13 @@ package-archive-priorities '(("melpa-stable" . 1)))
 (setq comint-scroll-to-bottom-on-output t) 
 (setq comint-move-point-for-output t)
 (setq ess-eval-visibly-p nil)
-(require 'ess-view)
-(require 'ess-site)
-(require 'ess-r-mode)
+
 (setq ess-use-auto-complete 'script-only)
 (define-key ac-completing-map "\t" 'ac-complete)
 
 (load "ess-autoloads.el")
 ;;We have "sbt" and "scala" in /usr/local/bin so we add this path to the PATH environment
 (setq-default explicit-shell-file-name "/bin/bash")
-(require 'ensime)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -74,7 +112,7 @@ package-archive-priorities '(("melpa-stable" . 1)))
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ess-view ess-R-data-view ess ensime scala-mode zenburn-theme))))
+    (helm ess-view ess-R-data-view ess ensime scala-mode zenburn-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
